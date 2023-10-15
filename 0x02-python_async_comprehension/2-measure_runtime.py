f6006c59d7208494c76a216f1b76ad958b80bf6f
RIKO-99 <rikohssn@gmail.com>
2023-10-14 17:42:16 -0700

    async gather for coroutine runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""""2-measure_runtime module file."""
+
+
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measure_runtime: Uses asyncio.gather to execute a coroutine four\
+        times and calculate the total runtime of the concurrency \
+            of the coroutines. Each coroutine takes the average time. \
+                since async_comprehension is asynchronously executed, \
+                    each random number will be generated and stored in the \
+                    list at O(1) which is the asyncio.sleep time. For \
+                        ten random numbers, it would be O(n) -> 10 \
+                            seconds(roughly).
+
+    Returns:
+        float: The mean time of the concurrency which is equivalent \
+            to the runtime of each coroutine.
+    """
+    strt_time: float = time.time()
+    await asyncio.gather(async_comprehension())
+    end_time: float = time.time()
+
+    total_time: float = end_time - strt_time
+    return total_time
