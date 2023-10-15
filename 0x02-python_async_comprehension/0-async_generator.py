b6aaaa36cdad8afe996e168e4e0649bc0e77eba0
RIKO-99 <rikohssn@gmail.com>
2023-10-14 17:32:15 -0700

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""0-async_generator module file."""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    async_generator: Generates a generator of integers.
+
+    Args:
+        No Argument.
+
+    Yields:
+        Generator[float, None, None]: A random float number through every\
+            loop. Waits for one minute before generating the next random float.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
